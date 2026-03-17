d95560d1258c677ae789412506e7ebbc2173c02a
Hitesh Pal <Hiteshpal267@gmail.com>
2026-03-17 09:05:22 +0530

    fix unused vars for vercel build

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import Board from "./components/Board";
-import Toolbar from "./components/Toolbar";
-import Toolbox from "./components/Toolbox";
-import BoardProvider from "./store/BoardProvider";
-import ToolboxProvider from "./store/ToolboxProvider";
-import { useState, useEffect } from "react";
+// import Board from "./components/Board";
+// import Toolbar from "./components/Toolbar";
+// import Toolbox from "./components/Toolbox";
+// import BoardProvider from "./store/BoardProvider";
+// import ToolboxProvider from "./store/ToolboxProvider";
+// import { useState, useEffect } from "react";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
